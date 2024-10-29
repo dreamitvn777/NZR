e9e012230453b53178520dff9854614f352233a5
dreamitvn777 <dreamitvn@gmail.com>
2024-10-30 01:45:58 +0700

    Create share.py

---
+++ share.py
@@ -0,0 +1,29 @@
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+# Thay đổi đường dẫn tới tệp JSON của bạn
+SERVICE_ACCOUNT_FILE = 'credentials.json'
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+# Xác thực
+creds = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+# Tạo dịch vụ Drive
+service = build('drive', 'v3', credentials=creds)
+
+# ID tài liệu bạn đã tạo
+document_id = '1IVZEMwNRKumK9Ys64W3a5Zd7et5vVYQt0vA17LNDCMc'
+
+# Chia sẻ tài liệu
+permission = {
+    'type': 'user',
+    'role': 'writer',  # Bạn có thể thay đổi thành 'reader' nếu chỉ muốn đọc
+    'emailAddress': 'bdpjournal@gmail.com'  # Thay đổi thành email gốc của bạn
+}
+
+service.permissions().create(
+    fileId=document_id,
+    body=permission,
+    fields='id'
+).execute()
